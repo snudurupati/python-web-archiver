7a30d052bbc3af640b0c0424c5f0c6db706b81ee
snudurupati <raghav.sreeram@gmail.com>
2018-10-01 12:07:09 +0200

    changed modules to create a single ftp connection in main and pass ftp connection object to these modules - creatindx, getfile, upload modules.

--- sdarch/src/monthly.py
+++ sdarch/src/monthly.py
@@ -1,12 +1,16 @@
 #Creates a directory for each new month on archives site via FTP
 import ftplib
-import datetime
+import datetime, os
 
 dtval = datetime.date.today()
-ftps = ftplib.FTP('www.sda-archives.com','gaurahari','Bhagavan108')
+#initialize ftp connection
+FTPUSER = os.environ.get("FTPUSER", '')
+FTPPASS = os.environ.get("FTPPASS", '')
+ftps = ftplib.FTP('www.sda-archives.com', FTPUSER, FTPPASS)
+print ftps
 mth = dtval.strftime('%b').lower()
 year = dtval.strftime('%Y')
 path = '/tftd/tftd/'+year+'/'+mth
-#print path
+print path
 ftps.mkd(path)
 ftps.quit()
